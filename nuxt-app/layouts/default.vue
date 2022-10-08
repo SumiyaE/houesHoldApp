420db1e33eaa1f31a61dd21187de9977b7746d43
sumiyaE <s_eguchi@codmon.co.jp>
2022-10-08 18:55:36 +0900

    サーバーとフロントを一つのリポジトリで管理するように修正

---
+++ nuxt-app/layouts/default.vue
@@ -0,0 +1,12 @@
+<template>
+  <Header/>
+    <div>
+      {{houseHoldItems}}
+      ss
+        <slot />
+    </div>
+  <Footer/>
+</template>
+<script lang="ts" setup>
+  const { data :houseHoldItems} = await useFetch('http://nest-back:4000/bought-items');
+</script>
